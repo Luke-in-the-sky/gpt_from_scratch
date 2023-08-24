992f06510ef3218ab24972facb7093241a9fa2c9
Luca Pietrobon <skywalker@fb.com>
2023-08-24 13:08:28 -0700

    addded a logger

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from modules import *
+import logging
 
 debugging = True
 
@@ -34,6 +35,12 @@ performing = Hyperparams(
 )
 
 hpars = debugging
+logging.basicConfig(
+    level=logging.DEBUG if debugging else logging.INFO,
+    format="%(asctime)s-%(levelname)s\t%(message)s",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger(__name__)
 
 # ----------
 # Tokenize and load datasets
@@ -41,16 +48,18 @@ hpars = debugging
 input_file = "input.txt"
 
 if not os.path.exists(input_file):
+    print("Downloading text..")
     download_data(destination_file=input_file)
 
 # read the file
+logger.info("Reading the text..")
 with open("input.txt", "r") as f:
     text = f.read()
 
 # set up vocab
 chars = sorted(list(set(text)))
 hpars.vocab_size = len(chars)
-print(f"{hpars.vocab_size=}")
+logger.info(f"{hpars.vocab_size=}")
 
 c2i = {c: i for i, c in enumerate(chars)}
 i2c = {i: c for c, i in c2i.items()}
@@ -59,6 +68,7 @@ decode = lambda d: "".join([i2c[i] for i in d])
 
 # tokenize and encode the text
 data = torch.tensor(encode(text), dtype=torch.long)
+
 
 # create dataset and dataloader
 n = int(len(data) * 0.9)
@@ -78,13 +88,14 @@ loader = DataLoader(
 # ----------
 
 m = MyGPT(hypers=hpars)
+logging.info(f'Model size: {sum([p.numel() for p in m.parameters()])}')
 if debugging:
     xb_debug, yb_debug = loader.get_batch(split="train")
     logits, loss = m(xb_debug, yb_debug)
-    print(f"{xb_debug.shape=}")
-    print(f"{logits.shape=}")
+    logger.debug(f"{xb_debug.shape=}")
+    logger.debug(f"{logits.shape=}")
     evaled_loss = evaluate_loss(m, loader, num_evals=10)
-    print(f"{evaled_loss=}")
+    logger.debug(f"{evaled_loss=}")
 
 
 # create optimizer
@@ -102,7 +113,7 @@ for i in range(hpars.training_steps):
     logits, loss = m(xb, targets=yb)
     if i % 100 == 0:
         out = evaluate_loss(m, loader)
-        print(f"{i}/{hpars.training_steps} {out=}")
+        logger.info(f"{i}/{hpars.training_steps} {out=}")
         losses.append(out)
 
     # back
